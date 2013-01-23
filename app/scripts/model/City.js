e561cbca034c8521b3976736db6859da846a3a78
Rishabh Rao <rishabhsrao@yahoo.com>
2013-01-23 16:48:53 +0530

    Adding City model.

---
+++ app/scripts/model/City.js
@@ -0,0 +1,19 @@
+define(["lib.use!lib.debug"], function(debug) {
+  "use strict";
+
+  var self = this;
+
+  self.name = "";
+
+  self.weather = {
+    temperature: {
+      average: 0,
+      maximum: 0,
+      minimum: 0
+    },
+
+    description: ""
+  };
+
+
+});
