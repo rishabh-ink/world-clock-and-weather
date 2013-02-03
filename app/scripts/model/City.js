d89ef90b83f8189f3b9795cc3ae4ffdff767e098
Rishabh Rao <rishabhsrao@yahoo.com>
2013-02-03 15:21:34 +0530

    Added applyMappings method using the other models.

--- app/scripts/model/City.js
+++ app/scripts/model/City.js
@@ -8,12 +8,26 @@
   "util.Storage",
   "util.Constants",
   "util.ErrorHandler",
+  "util.Mapper",
+  "model.weather.Astronomy",
+  "model.weather.Atmosphere",
+  "model.weather.Condition",
+  "model.weather.Forecast",
+  "model.weather.Location",
+  "model.weather.Wind"
   "knockout"
 ], function(
   debug,
   Storage,
   Constants,
   ErrorHandler,
+  Mapper,
+  Astronomy,
+  Atmosphere,
+  Condition,
+  Forecast,
+  Location,
+  Wind,
   ko
 ) {
   "use strict";
@@ -21,89 +35,52 @@
   var City = function() {
     var self = this;
 
-    self.info = {
-      name: ko.observable(""),
-      
-      coordinates: {
-        latitude: ko.observable(""),
-        longitude: ko.observable("")
+    City.prototype.init = function() {
+      debug.log("model.City", "init");
+
+      self.weather = {
+        location: Location.create(),
+        wind: Wind.create(),
+        atmosphere: Atmosphere.create(),
+        astronomy: Astronomy.create(),
+        condition: Condition.create(),
+        forecast: Forecast.create()
+      };
+    };
+
+    City.prototype.applyMappings = function(data) {
+      debug.log("model.city", "applyMappings", { data: data });
+
+      if(data) {
+        if("undefined" !== typeof (data['yweather:location'])) {
+          self.weather.location.applyMappings(data['yweather:location']);
+        }
+
+        if("undefined" !== typeof (data['yweather:wind'])) {
+          self.weather.wind.applyMappings(data['yweather:wind']);
+        }
+
+        if("undefined" !== typeof (data['yweather:atmosphere'])) {
+          self.weather.atmosphere.applyMappings(data['yweather:atmosphere']);
+        }
+
+        if("undefined" !== typeof (data['yweather:astronomy'])) {
+          self.weather.astronomy.applyMappings(data['yweather:astronomy']);
+        }
+
+        if("undefined" !== typeof (data.item)) {
+          if("undefined" !== typeof (data.item['yweather:condition'])) {
+            self.weather.condition.applyMappings(data.item['yweather:condition']);
+          }
+
+          if("undefined" !== typeof (data.item['yweather:forecast'])) {
+            self.weather.forecast.applyMappings(data.item['yweather:forecast']);
+          }
+        }
       }
     };
 
-    self.timezone = {
-      request: {
-        query: ko.observable(""),
-        type: ko.observable("")
-      },
-
-      time_zone: {
-        localtime: ko.observable(""),
-        utcOffset: ko.observable("")
-      },
-
-      description: ko.observable(""),
-      title: ko.observable("")
-    };
-
-    self.weather = {
-      current_condition: {
-        cloudcover: ko.observable(""),
-        humidity: ko.observable(""),
-        observation_time: ko.observable(""),
-        precipMM: ko.observable(""),
-        pressure: ko.observable(""),
-        temp_C: ko.observable(""),
-        temp_F: ko.observable(""),
-        visibility: ko.observable(""),
-        weatherCode: ko.observable(""),
-        weatherDesc: {
-            value: ko.observable("")
-        },
-        weatherIconUrl: {
-            value: ko.observable("")
-        },
-        winddir16Point: ko.observable(""),
-        winddirDegree: ko.observable(""),
-        windspeedKmph: ko.observable(""),
-        windspeedMiles: ko.observable("")
-      },
-
-      request: {
-        query: ko.observable(""),
-        type: ko.observable("")
-      }
-    };
-
-    City.prototype.applyTimezoneMappings = function(data) {
-      debug.log("model.city.applyTimezoneMappings", data);
-
-      self.timezone.request.query(data.value.items[0].request.query);
-      self.timezone.request.type(data.value.items[0].request.type);
-      self.timezone.time_zone.localtime(data.value.items[0].time_zone.localtime);
-      self.timezone.time_zone.utcOffset(data.value.items[0].time_zone.utcOffset);
-    };
-
-    City.prototype.applyWeatherMappings = function(data) {
-      debug.log("model.city.applyWeatherMappings", data);
-      
-      self.weather.current_condition.cloudcover(data.value.items[0].current_condition.cloudcover);
-      self.weather.current_condition.humidity(data.value.items[0].current_condition.humidity);
-      self.weather.current_condition.observation_time(data.value.items[0].current_condition.observation_time);
-      self.weather.current_condition.precipMM(data.value.items[0].current_condition.precipMM);
-      self.weather.current_condition.pressure(data.value.items[0].current_condition.pressure);
-      self.weather.current_condition.temp_C(data.value.items[0].current_condition.temp_C);
-      self.weather.current_condition.temp_F(data.value.items[0].current_condition.temp_F);
-      self.weather.current_condition.visibility(data.value.items[0].current_condition.visibility);
-      self.weather.current_condition.weatherCode(data.value.items[0].current_condition.weatherCode);
-      self.weather.current_condition.weatherDesc.value(data.value.items[0].current_condition.weatherDesc.value);
-      self.weather.current_condition.weatherIconUrl.value(data.value.items[0].current_condition.weatherIconUrl);
-      self.weather.current_condition.winddir16Point(data.value.items[0].current_condition.winddir16Point);
-      self.weather.current_condition.winddirDegree(data.value.items[0].current_condition.winddirDegree);
-      self.weather.current_condition.windspeedKmph(data.value.items[0].current_condition.windspeedKmph);
-      self.weather.current_condition.windspeedMiles(data.value.items[0].current_condition.windspeedMiles);
-      self.weather.request.query(data.value.items[0].request.query);
-      self.weather.request.type(data.value.items[0].request.type);
-    };
+    self.init();
 
     return self;
   };
